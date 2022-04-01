8e194d5014c8d1090ed5cf91fb1eeae192d28925
samyoung-dsci <samuel.young.work@gmail.com>
2022-04-01 09:31:56 +0100

    Increment version number and correct homepage url

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="clock_plot",
-    version="0.1.3",
+    version="0.1.4",
     author="Samuel Young",
     author_email="samuel.young.work@gmail.com",
     description="This package provides a simple way to visualize patterns in timeseries data mapping 24 hours onto a polar plot",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/name/clock_plot",
+    url="https://github.com/esc-data-science/clock_plot",
     project_urls={
-        "Bug Tracker": "https://github.com/name/clock_plot/issues",
+        "Bug Tracker": "https://github.com/esc-data-science/clock_plot/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
